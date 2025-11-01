889e43bd8ff3edec631045e77abb0ad95b3f1a2b
Miguel Gallego Martín <miguel.0.1.1.2.3.5.8@gmail.com>
2025-11-01 23:15:58 +0100

    Initial Commit

---
+++ SD_Movies/SD_MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SD_MoviesApp.swift
+//  SD_Movies
+//
+//  Created by Miguel Gallego on 1/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SD_MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
