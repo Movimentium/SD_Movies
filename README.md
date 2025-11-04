81aad437d168d317ade698dd84bf822155e9c86c
Miguel Gallego Martín <miguel.0.1.1.2.3.5.8@gmail.com>
2025-11-04 21:45:48 +0100

    AddReviewScreen done

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # SD_Movies
 
+A SwiftData app example based on a tutorial by M.A. (That can be improved)
+
 ## Description
 
 Swift Data iOS app. 
--- SD_Movies/Screens/AddReviewScreen.swift
+++ SD_Movies/Screens/AddReviewScreen.swift
@@ -5,13 +5,56 @@ import SwiftUI
 
 struct AddReviewScreen: View {
     let movie: Movie
-    
+
+    @Environment(\.modelContext) private var modelCtx
+    @Environment(\.dismiss) private var dismiss
+    @State private var subject: String = ""
+    @State private var description: String = ""
     
     var body: some View {
-        Text("Hello, World!")
+        Form {
+            TextField("Subject", text: $subject)
+            TextField("Description", text: $description)
+        }
+        .navigationTitle("Review: \(movie.title)")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button { dismiss() } label: {
+                    Image(systemName: "xmark")
+                }
+            }
+            ToolbarItem(placement: .topBarTrailing) {
+                Button("Save") { saveReview() }
+                    .disabled(!isFormValid)
+            }
+        }
     }
+    
+    
+    // MARK: - Logic
+    private func saveReview() {
+        let review = Review(subject: subject, body: description)  //IDLI
+        review.movie = movie
+        modelCtx.insert(review)
+        do {                               // IDLI: repeated code
+            try modelCtx.save()
+            movie.reviews?.append(review)  // is this necessary??
+        } catch {
+            print(error.localizedDescription)
+        }
+        dismiss()
+    }
+    
+    private var isFormValid: Bool {
+        !subject.isEmptyOrWhiteSpace && !description.isEmptyOrWhiteSpace
+    }
+    
 }
 
 #Preview {
-    AddReviewScreen(movie: Movie.movies.first!)
+    NavigationStack {
+        AddReviewScreen(movie: Movie.movies.first!)
+            .modelContainer(for: [Movie.self, Review.self])
+    }
 }
