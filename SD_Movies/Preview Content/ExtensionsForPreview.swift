98cfd209e80b51c09a874670c6b21bd26ebc489f
Miguel Gallego Martín <miguel.0.1.1.2.3.5.8@gmail.com>
2025-11-06 15:56:36 +0100

    ActorSelectionView done

--- SD_Movies/Preview Content/ExtensionsForPreview.swift
+++ SD_Movies/Preview Content/ExtensionsForPreview.swift
@@ -2,6 +2,7 @@
 //  SD_Movies
 //  Created by Miguel Gallego on 4/11/25.
 import Foundation
+import SwiftData
 
 extension Movie {
     static let movies = [
@@ -17,9 +18,17 @@ extension Review {
     ]
 }
 
-extension Actor {
+extension SD_Movies.Actor {
     static let actors = [
         Actor(name: "Carrie-Anne Moss"), Actor(name: "Michael Keaton"),
         Actor(name: "Jack Nicholson"), Actor(name: "Guy Pearce")
     ]
+    
+    @MainActor
+    static let previewContainer = {
+        let modelConfig = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = try! ModelContainer(for: Actor.self, configurations: modelConfig)
+        Actor.actors.forEach { container.mainContext.insert($0) }
+        return container
+    }()
 }
--- SD_Movies/Views/ActorSelectionView.swift
+++ SD_Movies/Views/ActorSelectionView.swift
@@ -5,13 +5,34 @@ import SwiftUI
 import SwiftData
 
 struct ActorSelectionView: View {
-    @Query private var actors: [Actor]
+    @Query(sort: \Actor.name, order: .forward) private var actors: [Actor]
+    @Binding var selectedActors: Set<Actor>
     
     var body: some View {
         List(actors) { actor in
-            
-            Text(actor.name)
+            HStack {
+                Image(systemName: selectedActors.contains(actor) ? "checkmark.square" : "square")
+                    
+                Text(actor.name)
+            }
+            .onTapGesture { onCheckActor(actor) }
+        }
+    }
+    
+    // MARK: - Logic
+    func onCheckActor(_ actor: Actor) {
+        if !selectedActors.contains(actor) {
+            selectedActors.insert(actor)
+        } else {
+            selectedActors.remove(actor)
         }
     }
 }
 
+
+#Preview {
+    NavigationStack {
+        ActorSelectionView(selectedActors: .constant(Set(Actor.actors) ))
+            .modelContainer(Actor.previewContainer)
+    }
+}
